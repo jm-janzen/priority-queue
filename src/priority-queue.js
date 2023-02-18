5cda582fa7e0ff8037b9e6625a85b947bcc6995f
JM <jmjanzen@gmail.com>
2023-02-18 16:11:39 -0500

    refactor: change PriorityQueue to a class
    Using the 'class' concept here makes the code much more readable and
    modern, and doesn't outwardly change the behaviour of any of our happy
    path functionality (we can see the proof of this since our tests at this
    revision still pass).
    
    Using a class also gives us all of the advantages of explicitly
    specifying 'prototype' like you did for free. Incidentally, this moves
    the 'add,' 'Pop,' and 'length' methods onto the prototype of this class,
    with no significant difference to usage.

--- src/priority-queue.js
+++ src/priority-queue.js
@@ -3,60 +3,63 @@
  * Items with a higher priority are dequeued before items with a lower priority.
  * Implemented as a hash of arrays where the hash keys are priority values.
  */
-function PriorityQueue() {
-	this.store = {};	// keys are priorities, values are arrays of elements
-	this.count = 0;
+class PriorityQueue {
+	constructor() {
+		this.store = {}; // keys are priorities, values are arrays of elements
+		this.count = 0;
+	}
 
 	// adds an item
 	// priority must be an integer (higher value has higher priority)
-	this.add = function(value, priority) {
+	add(value, priority) {
 		if (this.store[priority] == undefined)
 			this.store[priority] = [];
 
 		this.store[priority].push(value);
 		this.count++;
-	};
+	}
 
 	// returns the oldest-added value with the highest priority
-	this.Pop = function() {
+	Pop() {
 		const maxKey = Math.max(Object.keys(this.store));
 		this.count--;
 		return this.store[maxKey].shift();
-	};
-
-	this.length = function() {
-		return this.count;
-	};
-}
-
-PriorityQueue.prototype.get_all_priorities = function() {
-	return Object.keys(this.store);
-};
-
-// iterates through all the queue elements in priority-then-FIFO order
-PriorityQueue.prototype.forEach = function(callback) {
-	var keys = Object.keys(this.store).sort();
-
-	for (var a = keys.length; a > 0; a--) {
-		for (var b = 0; b < this.store[a].length; b++)
-			callback(this.store[a][b]);
 	}
-};
 
-PriorityQueue.prototype.changePriority = function(value, newPriority) {
-	var foundItem = false;
+	length() {
+		return this.count;
+	}
 
-	this.store.forEach(function(bucket) {
-		bucket.forEach(function(item, index) {
-			if (item === value) {
-				bucket.splice(index, 1);  // remove the item
-				this.add(value, newPriority);
-				foundItem = true;
-				return false;  // early exit from forEach
-			}
+	get_all_priorities() {
+		return Object.keys(this.store);
+	}
+
+	// iterates through all the queue elements in priority-then-FIFO order
+	forEach(callback) {
+		var keys = Object.keys(this.store).sort();
+
+		for (var a = keys.length; a > 0; a--) {
+			for (var b = 0; b < this.store[a].length; b++)
+				callback(this.store[a][b]);
+		}
+	}
+
+	changePriority(value, newPriority) {
+		var foundItem = false;
+
+		this.store.forEach(function (bucket) {
+			bucket.forEach(function (item, index) {
+				if (item === value) {
+					bucket.splice(index, 1); // remove the item
+					this.add(value, newPriority);
+					foundItem = true;
+					return false; // early exit from forEach
+				}
+			});
+			if (foundItem)
+				return false;
 		});
-		if (foundItem) return false;
-	});
-};
+	}
+}
 
 module.exports = { PriorityQueue };
